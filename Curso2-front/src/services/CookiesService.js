f0bb8988f845badb4fd416084c0b38ac2da05d1c
agustin garcia maggi <agustingmaggi@gmail.com>
2024-03-24 17:24:10 +0100

    cambiando algo para ver si funciona

--- Curso2-front/src/services/CookiesService.js
+++ Curso2-front/src/services/CookiesService.js
@@ -2,13 +2,20 @@ import AxiosClient from "./AxiosClient";
 
 export default class CookiesService {
     constructor() {
-        this.client = new AxiosClient()
+        this.client = new AxiosClient();
     }
 
-    getCookies = () => {
-        const requestInfo = {
-            url: `https://supermarket-syyv.onrender.com/`
+    getCookies = async () => {
+        try {
+            const response = await this.client.makeGetRequest('https://supermarket-syyv.onrender.com/');
+            // Si necesitas acceder a las cookies, puedes hacerlo a través de la propiedad "headers" de la respuesta
+            const cookies = response.headers['set-cookie'];
+            // Ahora puedes hacer lo que necesites con las cookies
+            console.log("Cookies recibidas:", cookies);
+            return response; // O devolver cualquier otro dato que necesites
+        } catch (error) {
+            console.error("Error al obtener las cookies:", error);
+            throw error; // Maneja el error según tus necesidades
         }
-        return this.client.makeGetRequest(requestInfo)
     }
 }
