626bc23c63f50e8cd588e289fe98bacc18b1ef17
agustin garcia maggi <agustingmaggi@gmail.com>
2024-03-24 18:50:41 +0100

    agregando with credenials true

--- Curso2-front/src/services/CookiesService.js
+++ Curso2-front/src/services/CookiesService.js
@@ -8,7 +8,7 @@ export default class CookiesService {
     getCookies = () => {
         const requestInfo = {
             url: `https://supermarket-syyv.onrender.com/`,
-            // { withCredentials: true }
+            withCredentials: true
         }
         return this.client.makeGetRequest(requestInfo)
     }
