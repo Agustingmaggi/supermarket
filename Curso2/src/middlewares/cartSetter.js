a80d2bf313bfa10bd4c40a676b95b251990046fc
agustin garcia maggi <agustingmaggi@gmail.com>
2024-03-24 22:04:46 +0100

    probando

--- Curso2/src/middlewares/cartSetter.js
+++ Curso2/src/middlewares/cartSetter.js
@@ -6,7 +6,7 @@ const cartSetter = async (req, res, next) => {
     if (!req.cookies.cart && !req.user) {
         const cart = await cartService.createCart();
         res.cookie('cart', cart._id.toString(), {
-            domain: 'https://supermarket-front.onrender.com/',
+            // domain: 'https://supermarket-front.onrender.com/',
             sameSite: 'none',
             secure: true
         })
